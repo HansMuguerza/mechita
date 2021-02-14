f08b36abae1d7c0a5b0b682e624c1fdc9c05cbcd
Hans Muguerza <hansmuguerza14@gmail.com>
2021-02-13 21:08:36 -0500

    update

--- index.js
+++ index.js
@@ -129,3 +129,19 @@ taskForm.addEventListener("submit", async (e) => {
     console.log(error);
   }
 });
+
+
+
+
+function sumar()
+{
+    const $total = document.getElementById('total');
+    let subtotal = 0;
+    [ ...document.getElementsByClassName( "monto" ) ].forEach( function ( element ) {
+        if(element.value !== '') {
+        subtotal += parseFloat(element.value);
+        }
+    });
+
+    $total.value = subtotal;
+}
